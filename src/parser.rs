d097ba2051b077c99dbe3d66d58c7a0b107c984f
Vanilla Viking <ashwinr2k2@gmail.com>
2023-05-26 11:08:13 +1000

    wip-refactor

--- src/parser.rs
+++ src/parser.rs
@@ -1,3 +1,41 @@
 pub mod parser {
+    use crate::tokenizer::tokenizer::{Tokens, TokenList};
+
+    enum Level {
+        ONE,
+        TWO,
+        THREE,
+    }
+
+    struct Bold {
+        text: String,
+    }
+
+    struct Italic {
+        text: String,
+    }
+
+    pub struct Heading {
+        level: Level,
+        text: String,
+    }
+
+    enum Sentences {
+        BOLD(Bold),
+        ITALIC(Italic),
+        NORMAL(String),
+    }
+
+    struct Paragraph {
+        sentences: Vec<Sentences>
+    }
+
+    pub fn parse(tokens: &TokenList) {
+        tokens.list.iter().for_each(|token| {
+            match token {
+                Tokens::HASHTAG => ()
+            }
+        })
+    }
 }
 
--- src/tokenizer.rs
+++ src/tokenizer.rs
@@ -1,10 +1,11 @@
 pub mod tokenizer {
     #[derive(Debug)]
     pub enum Tokens {
-        Heading1(String),
-        Heading2(String),
-        Heading3(String),
-        Normal(String),
+        HASHTAG,
+        ASTERISK,
+        UNDERSCORE,
+        HYPHEN,
+        TEXT(String),
     }
 
     #[derive(Debug)]
@@ -15,69 +16,27 @@ pub mod tokenizer {
     impl TokenList {
 
         pub fn tokenize(&mut self, md_line: &str) {
-            match (md_line.chars().nth(0), md_line.chars().nth(1), md_line.chars().nth(2)) {
-                (Some('#'), Some('#'), Some('#')) =>    self.list.push(Tokens::Heading3(String::from(&md_line[3..]))),
-                (Some('#'), Some('#'), _) =>            self.list.push(Tokens::Heading2(String::from(&md_line[2..]))),
-                (Some('#'), _, _) =>                    self.list.push(Tokens::Heading1(String::from(&md_line[1..]))),
-                (_,_,_) =>                              if !md_line.is_empty() { self.list.push(Tokens::Normal(String::from(md_line))) } else {()},
-            }
+            md_line.chars().for_each(|char| {
+                match char {
+                    '#' => self.list.push(Tokens::HASHTAG),
+                    '*' => self.list.push(Tokens::ASTERISK),
+                    '_' => self.list.push(Tokens::UNDERSCORE),
+                    '-' => self.list.push(Tokens::HYPHEN),
+                    _ => {
+                        if let Some(Tokens::TEXT(s)) = self.list.last() {
+                            self.list.pop();
+                            self.list.push(Tokens::TEXT(String::from("{s}{char}")))
+                        }
+                    }
+                }
+            })
         }
 
-        pub fn parse(&self) -> String {
-            let mut html_result = String::from("<html>\n");
-
-            self.list.iter().for_each(|token| {
-                match token {
-                    Tokens::Heading1(txt) =>    html_result.push_str(&format!("<h1>{}</h1>\n", txt)),
-                    Tokens::Heading2(txt) =>    html_result.push_str(&format!("<h2>{}</h2>\n", txt)),
-                    Tokens::Heading3(txt) =>    html_result.push_str(&format!("<h3>{}</h3>\n", txt)),
-                    Tokens::Normal(txt) =>      html_result.push_str(&format!("<p>{}</p>\n", txt)),
-                };
-            });
-
-            html_result.push_str("</html>\n");
-
-            html_result
-        }
     }
 
 }
 
 #[cfg(test)]
 mod tests {
-    use super::tokenizer::TokenList;
-    use super::tokenizer::Tokens;
-
-    #[test]
-    fn heading_works() {
-        let mut tokens = TokenList {
-            list: vec![],
-        };
-
-        tokens.tokenize("# This is a heading");
-        tokens.tokenize("## This is a heading2");
-        tokens.tokenize("### This is a heading3");
-        assert_eq!(tokens.list.len(), 3);
-    }
-
-    #[test]
-    fn heading_shouldnt_run() {
-        let mut tokens = TokenList {
-            list: vec![],
-        };
-
-        tokens.tokenize("This is not a heading");
-        // TODO: fix up this test
-        assert_eq!(tokens.list.len(), 1);
-    }
-
-    #[test]
-    fn parse_headings() {
-        let tokens = TokenList {
-            list: vec![Tokens::Heading1(String::from("Hello, World"))],
-        };
-        
-        assert_eq!(&tokens.parse(), "<html>\n<h1>Hello, World</h1>\n</html>\n");
-    }
 }
 
